d3b430ef772db178d52dc25b8ec2cfa8283f8c49
Kelly Chhe <kellykchhe@gmail.com>
2022-05-18 10:12:35 -0400

    code warrrssszzz

---
+++ 8kyu/trainingJS17.js
@@ -0,0 +1,17 @@
+// Task
+// Coding in function firstToLast, function accept 2 parameters:str and c. str is a string. c is a char.
+
+// Please return the gap between the first position of c and the last position of c.
+
+// If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
+
+function firstToLast(str,c){
+    let firstIndex = str.indexOf(c)
+    let lastIndex = str.lastIndexOf(c)
+    if (firstIndex === -1){
+      return firstIndex
+    }
+    else{
+    return lastIndex - firstIndex
+    }
+  }

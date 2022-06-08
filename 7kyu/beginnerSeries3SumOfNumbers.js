4e82e5e75fc735c000c8a8074d9dd067c3c2d44c
Kelly Chhe <kellykchhe@gmail.com>
2022-06-07 21:42:56 -0400

    code war

---
+++ 7kyu/beginnerSeries3SumOfNumbers.js
@@ -0,0 +1,20 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+function getSum( a,b )
+{
+  let sum = 0
+  if (a > b){
+    for(let i = b; i <= a; i++){
+      sum += i
+    }
+  } else if (a < b){
+    for (let i = a; i <= b; i++){
+      sum += i
+    }
+  } else{
+    sum = a
+  }
+  return sum
+}
